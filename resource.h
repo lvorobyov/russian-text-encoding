602f9697d3253a076b3d2c9d685e139cd93eda31
Лев Воробьёв <lev.vorobjev@rambler.ru>
2017-11-27 14:46:14 +0300

    Add menu section 'Edit'

--- resource.h
+++ resource.h
@@ -7,5 +7,11 @@
 #define IDM_ITEM2                               40001
 #define IDM_ITEM3                               40002
 #define IDM_ITEM4                               40003
-#define IDM_ITEM5                               40004
-#define IDM_ITEM6                               40005
+#define IDM_ITEMSTEGO                           40004
+#define IDM_ITEMUNSTEGO                         40005
+#define IDM_ITEMUNDO                            40006
+#define IDM_ITEMREDO                            40007
+#define IDM_ITEMCUT                             40008
+#define IDM_ITEMCOPY                            40009
+#define IDM_ITEMPASTE                           40010
+#define IDM_ITEMALL                             40011
